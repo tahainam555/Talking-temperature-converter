958a3d2c1fb8ee3317dc49a7cc5d8b8ee357825c
tahainam555 <tahainam879@gmail.com>
2024-08-03 00:27:38 +0500

    Temperature convertor

--- index.js
+++ index.js
@@ -3,19 +3,33 @@ const toFahrenheit=document.getElementById("toFahrenheit");
 const toCelcius=document.getElementById("toCelcius");
 const result=document.getElementById("result");
 let temp;
+let input;
+
+function speak(text) {
+    // var text = `${input} after conversion is ${temp}`;
+    var utterance = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(utterance);
+  }
 
 function convert(){
     if(toFahrenheit.checked){
+        input = Number(textBox.value);
         temp = Number(textBox.value);
         temp = temp * 9 / 5 + 32 ;
         result.textContent = temp.toFixed(1) + "°F";        //toFixed() for precision  
+        speak(`${input} degree Celcius after conversion is ${temp} degree Fahrenheit`);
     }
     else if(toCelcius.checked){
+        input = Number(textBox.value);
         temp = Number(textBox.value);
         temp = (temp - 32) * (5/9) ;
         result.textContent = temp.toFixed(1) + "°C";        //toFixed() for precision
+        speak(`${input} degree Fahrenheit after conversion is ${temp} degree Celcius`);
     }
     else{
         result.textContent = "Select a Unit!";
+        speak(`Please select a unit`);
     }
 }
+
+
